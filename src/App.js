af568ca1b439e282b5cb0b791bd4b3e33a9b1c67
Diyar Faraj <diyar.faraj@gmail.com>
2020-08-08 21:21:58 +0200

    api key fix

--- src/App.js
+++ src/App.js
@@ -2,30 +2,28 @@ import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import unirest from "unirest";
 import "./App.css";
-require("dotenv").config();
 
 function App() {
   const [term, setTerm] = useState([]);
+
+  const api_url =
+    "https://listen-api.listennotes.com/api/v2/search?q=python&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30000&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0";
 
   useEffect(() => {
     getPodcasts();
   }, []);
 
-  const api_url =
-    "https://listen-api.listennotes.com/api/v2/search?q=python&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30000&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0";
-
   const getPodcasts = async () => {
     const response = await fetch(api_url, {
       method: "GET",
-      headers: { "X-ListenAPI-Key": process.env.REACT_APP_API_KEY },
+      headers: { "X-ListenAPI-Key": `${process.env.REACT_APP_API_KEY}` },
     });
     const data = await response.json();
 
-    console.log(data.results);
+    //console.log(data.results);
     setTerm(data.results);
   };
 
-  //console.log(term);
   term.forEach((el) => console.log(el.podcast_title_original));
 
   return (
